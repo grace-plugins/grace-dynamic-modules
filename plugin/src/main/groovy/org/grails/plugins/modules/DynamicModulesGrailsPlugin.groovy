7f5a79ca7cab140a2e5a845394436b0e28e0735d
Michael Yan <rain@rainboyan.com>
2026-01-30 18:59:43 +0800

    Release v1.0.0

--- plugin/src/main/groovy/org/grails/plugins/modules/DynamicModulesGrailsPlugin.groovy
+++ plugin/src/main/groovy/org/grails/plugins/modules/DynamicModulesGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2025 the original author or authors.
+ * Copyright 2022-2026 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,7 +31,7 @@ import grails.util.GrailsUtil
 @CompileStatic
 class DynamicModulesGrailsPlugin extends DynamicPlugin {
 
-    def version = '1.0.0-SNAPSHOT'
+    def version = '1.0.0'
     def grailsVersion = "2023.0.0 > *"
 
     def providedModules = [
